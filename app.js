455bce238c616585d05fc44d3bc4af3f730a96c7
yusufalp <alpyusuf007@gmail.com>
2024-12-12 21:19:04 -0800

    create a server

---
+++ app.js
@@ -0,0 +1,49 @@
+import "dotenv/config";
+
+import express from "express";
+import cors from "cors";
+
+import connectDB from "./config/database.js";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+
+connectDB();
+
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:5173",
+      "http://localhost:8080",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  console.error("An error occurred:", err);
+
+  if (err.code === 11000) {
+    return res.status(400).json({
+      error: { message: "There was a problem when signing up." },
+    });
+  }
+
+  return res.status(err.code || 500).json({
+    error: { message: err.message || "Internal server error." },
+  });
+});
+
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () => {
+  console.log(`
+    Server is listening on port ${PORT}. 
+    http://localhost:${PORT}
+    `);
+});
