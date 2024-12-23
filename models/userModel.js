5c5aa25b7e0b6178e414daae5b629cc7b7871e46
yusufalp <alpyusuf007@gmail.com>
2024-12-23 15:18:51 -0800

    add a field for user last login date

--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,7 @@ const userSchema = new Schema(
       trim: true,
     },
     password: { type: String, required: true },
+    lastLogin: { type: Date, default: null },
   },
   { timestamps: true }
 );
